f472e31cfb0b660e4e8508912bc0535c4914f039
David Perfors <dnperfors@gmail.com>
2020-03-05 22:13:45 +0100

    Creates basic implmentation of HttpResponseMessageBuilder

---
+++ src/HttpClientTestHelpers/HttpResponseMessageBuilder.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+
+namespace HttpClientTestHelpers
+{
+    public class HttpResponseMessageBuilder
+    {
+        private readonly HttpResponseMessage httpResponseMessage = new HttpResponseMessage();
+
+        public HttpResponseMessageBuilder WithVersion(Version httpVersion)
+        {
+            httpResponseMessage.Version = httpVersion;
+            return this;
+        }
+
+        public HttpResponseMessageBuilder WithStatusCode(HttpStatusCode statusCode)
+        {
+            httpResponseMessage.StatusCode = statusCode;
+            return this;
+        }
+
+        public HttpResponseMessageBuilder WithHeader(string header, string value)
+        {
+            if (string.IsNullOrEmpty(header))
+            {
+                throw new ArgumentNullException(nameof(header));
+            }
+
+            httpResponseMessage.Headers.Add(header, value);
+            return this;
+        }
+
+        public HttpResponseMessageBuilder WithContent(HttpContent content)
+        {
+            httpResponseMessage.Content = content;
+            return this;
+        }
+
+        public HttpResponseMessageBuilder WithRequestMessage(HttpRequestMessage requestMessage)
+        {
+            httpResponseMessage.RequestMessage = requestMessage;
+            return this;
+        }
+
+        public HttpResponseMessage Build()
+        {
+            return httpResponseMessage;
+        }
+    }
+}
---
+++ test/HttpClientTestHelpers.Tests/HttpResponseMessageBuilderTests.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using Xunit;
+
+namespace HttpClientTestHelpers.Tests
+{
+    public class HttpResponseMessageBuilderTests
+    {
+        [Fact]
+        public void Build_ReturnsEmptyHttpResponseMessage()
+        {
+            var sut = new HttpResponseMessageBuilder();
+
+            var result = sut.Build();
+
+            Assert.NotNull(result);
+            Assert.Equal(HttpVersion.Version11, result.Version);
+            Assert.Equal(HttpStatusCode.OK, result.StatusCode);
+            Assert.Empty(result.Headers);
+            Assert.Null(result.Content);
+            Assert.Null(result.RequestMessage);
+        }
+
+        [Fact]
+        public void WithVersion_CreatesHttpResponseMessageWithCorrectVersion()
+        {
+            var sut = new HttpResponseMessageBuilder();
+
+            var result = sut.WithVersion(HttpVersion.Version20).Build();
+
+            Assert.Equal(HttpVersion.Version20, result.Version);
+        }
+
+        [Fact]
+        public void WithStatusCode_CreatesHttpResponseMessageWithCorrectStatusCode()
+        {
+            var sut = new HttpResponseMessageBuilder();
+
+            var result = sut.WithStatusCode(HttpStatusCode.BadRequest).Build();
+
+            Assert.Equal(HttpStatusCode.BadRequest, result.StatusCode);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        public void WithHeader_NullOrEmptyName_ThrowsArgumentException(string headerName)
+        {
+            var sut = new HttpResponseMessageBuilder();
+
+            var exception = Assert.Throws<ArgumentNullException>(() => sut.WithHeader(headerName, "value"));
+            Assert.Equal("header", exception.ParamName);
+        }
+
+        [Fact]
+        public void WithHeader_CreatesHttpResponseMessageWithHeaderAddedToTheList()
+        {
+            var sut = new HttpResponseMessageBuilder();
+
+            var result = sut.WithHeader("Location", "https://example.com/").Build();
+
+            Assert.Equal("https://example.com/", result.Headers.GetValues("Location").Single());
+        }
+
+        [Fact]
+        public void WithContent_CreatesHttpResponseMessageWithContent()
+        {
+            var sut = new HttpResponseMessageBuilder();
+            using var content = new StringContent(string.Empty);
+
+            var result = sut.WithContent(content).Build();
+
+            Assert.Same(content, result.Content);
+        }
+
+        [Fact]
+        public void WithRequestMessage_CreatesHttpResponseMessagaeWithRequestMessage()
+        {
+            var sut = new HttpResponseMessageBuilder();
+            using var requestMessage = new HttpRequestMessage();
+
+            var result = sut.WithRequestMessage(requestMessage).Build();
+
+            Assert.Same(requestMessage, result.RequestMessage);
+        }
+    }
+}
