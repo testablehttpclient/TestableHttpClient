d7ee9081ca2a516b27a75711242f7fa9d2af0256
David Perfors <dnperfors@gmail.com>
2020-03-10 20:45:07 +0100

    Added WithJsonContent asserter.

--- src/HttpClientTestHelpers/HttpRequestMessageAsserter.cs
+++ src/HttpClientTestHelpers/HttpRequestMessageAsserter.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Text.Json;
 
 namespace HttpClientTestHelpers
 {
@@ -267,6 +268,17 @@ namespace HttpClientTestHelpers
             }
 
             return With(x => x.HasContent(pattern), $"content '{pattern}'");
+        }
+
+        /// <summary>
+        /// Asserts wheter requests are made with specific json content.
+        /// </summary>
+        /// <param name="jsonObject">The object representation of the expected request content.</param>
+        /// <returns>The <seealso cref="HttpRequestMessageAsserter"/> for further assertions.</returns>
+        public HttpRequestMessageAsserter WithJsonContent(object? jsonObject)
+        {
+            var jsonString = JsonSerializer.Serialize(jsonObject);
+            return With(x => x.HasContent(jsonString) && x.HasContentHeader("Content-Type", "application/json*"), $"json content '{jsonString}'");
         }
 
         /// <summary>
--- test/HttpClientTestHelpers.Tests/HttpRequestMessageAsserterTests.cs
+++ test/HttpClientTestHelpers.Tests/HttpRequestMessageAsserterTests.cs
@@ -606,6 +606,47 @@ namespace HttpClientTestHelpers.Tests
         }
 
         [Fact]
+        public void WithJsonContent_RequestWithMatchingContent_ReturnsHttpRequestMessageAsserter()
+        {
+            var request = new HttpRequestMessage
+            {
+                Content = new StringContent("null", Encoding.UTF8, "application/json")
+            };
+            var sut = new HttpRequestMessageAsserter(new[] { request });
+
+            var result = sut.WithJsonContent(null);
+
+            Assert.NotNull(result);
+            Assert.IsType<HttpRequestMessageAsserter>(result);
+        }
+
+        [Fact]
+        public void WithJsonContent_RequestWithDifferentContent_ThrowsHttpRequestMessageAssertionExceptionWithSpecificMessage()
+        {
+            var request = new HttpRequestMessage
+            {
+                Content = new StringContent("{}", Encoding.UTF8, "application/json")
+            };
+            var sut = new HttpRequestMessageAsserter(new[] { request });
+
+            var exception = Assert.Throws<HttpRequestMessageAssertionException>(() => sut.WithJsonContent(null));
+            Assert.Equal("Expected at least one request to be made with json content 'null', but no requests were made.", exception.Message);
+        }
+
+        [Fact]
+        public void WithJsonContent_RequestWithDifferentContentType_ThrowsHttpRequestMessageAssertionExceptionWithSpecificMessage()
+        {
+            var request = new HttpRequestMessage
+            {
+                Content = new StringContent("null", Encoding.UTF8)
+            };
+            var sut = new HttpRequestMessageAsserter(new[] { request });
+
+            var exception = Assert.Throws<HttpRequestMessageAssertionException>(() => sut.WithJsonContent(null));
+            Assert.Equal("Expected at least one request to be made with json content 'null', but no requests were made.", exception.Message);
+        }
+
+        [Fact]
         public void Times_ValueLessThan0_ThrowsArgumentException()
         {
             var sut = new HttpRequestMessageAsserter(Enumerable.Empty<HttpRequestMessage>());
