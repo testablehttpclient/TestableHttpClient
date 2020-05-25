516cee741bf69fc40b5ae93b3c2c651d1c84975d
David Perfors <dnperfors@gmail.com>
2020-05-25 08:00:51 +0200

    Add CreateClient method to create a simple HttpClient.

---
+++ src/TestableHttpClient/TestableHttpMessageHandlerExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+
+namespace TestableHttpClient
+{
+    public static class TestableHttpMessageHandlerExtensions
+    {
+        /// <summary>
+        /// Create an <seealso cref="HttpClient"/> configured with the TestableHttpMessageHandler.
+        /// </summary>
+        /// <param name="handler">The TestableHttpMessageHandler to set on the client.</param>
+        /// <returns>An HttpClient configure with the TestableHttpMessageHandler.</returns>
+        /// <exception cref="ArgumentNullException">The `handler` is `null`</exception>
+        /// <remarks>Using this method is equivalent to `new HttClient(handler)`.</remarks>
+        public static HttpClient CreateClient(this TestableHttpMessageHandler handler)
+        {
+            if (handler is null)
+            {
+                throw new ArgumentNullException(nameof(handler));
+            }
+
+            return new HttpClient(handler);
+        }
+    }
+}
---
+++ test/TestableHttpClient.Tests/TestableHttpMessageHandlerExtensionsTests/CreateClient.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Reflection;
+using System.Text;
+
+using Xunit;
+
+namespace TestableHttpClient.Tests
+{
+    public partial class TestableHttpMessageHandlerExtensionsTests
+    {
+#nullable disable
+        [Fact]
+        public void CreateClient_NullTestableHttpMessageHandler_ThrowsArgumentNullException()
+        {
+            TestableHttpMessageHandler sut = null;
+
+            var exception = Assert.Throws<ArgumentNullException>(() => sut.CreateClient());
+            Assert.Equal("handler", exception.ParamName);
+        }
+#nullable restore
+
+        [Fact]
+        public void CreateClient_CorrectTestableHttpMessageHandler_AddsHandlerToHttpClient()
+        {
+            using var sut = new TestableHttpMessageHandler();
+
+            using var client = sut.CreateClient();
+
+            var handler = GetPrivateHandler(client);
+
+            Assert.Same(sut, handler);
+        }
+
+        private static object? GetPrivateHandler(HttpClient client)
+        {
+            var handlerField = client.GetType().BaseType?.GetField("_handler", BindingFlags.Instance | BindingFlags.NonPublic);
+            if(handlerField == null)
+            {
+                Assert.True(false, "Can't find the private _handler field on HttpClient.");
+                return null;
+            }
+            return handlerField.GetValue(client);
+        }
+    }
+}
