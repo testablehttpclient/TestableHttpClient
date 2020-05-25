53db1228b62544558283e6cbf0ad8a537d2829bd
David Perfors <dnperfors@gmail.com>
2020-05-25 20:42:23 +0200

    Add CreateClient methods to TestableHttpMessageHandler
    The following methods are added:
    
    - `TestableHttpMessageHandlerExtension.CreateClient(this TestableHttpMessageHandler)`
    - `TestableHttpMessageHandlerExtension.CreateClient(this TestableHttpMessageHandler, Action<HttpClient>)`
    
    Fixes #21.

---
+++ src/TestableHttpClient/TestableHttpMessageHandlerExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net.Http;
+
+namespace TestableHttpClient
+{
+    public static class TestableHttpMessageHandlerExtensions
+    {
+        /// <summary>
+        /// Create an <seealso cref="HttpClient"/> configured with the TestableHttpMessageHandler.
+        /// </summary>
+        /// <param name="handler">The TestableHttpMessageHandler to set on the client.</param>
+        /// <returns>An HttpClient configure with the TestableHttpMessageHandler.</returns>
+        /// <exception cref="ArgumentNullException">The `handler` is `null`</exception>
+        /// <remarks>Using this method is equivalent to `new HttClient(handler)`.</remarks>
+        public static HttpClient CreateClient(this TestableHttpMessageHandler handler)
+        {
+            return CreateClient(handler, _ => { });
+        }
+
+        /// <summary>
+        /// Create and configure an <seealso cref="HttpClient"/> configured with the TestableHttpMessageHandler.
+        /// </summary>
+        /// <param name="handler">The TestableHttpMessageHandler to set on the client.</param>
+        /// <param name="configureClient">A delegate that is used to configure an HttpClient</param>
+        /// <returns>An HttpClient configure with the TestableHttpMessageHandler.</returns>
+        /// <exception cref="ArgumentNullException">The `handler` or `configureClient` is `null`</exception>
+        public static HttpClient CreateClient(this TestableHttpMessageHandler handler, Action<HttpClient> configureClient)
+        {
+            if (handler is null)
+            {
+                throw new ArgumentNullException(nameof(handler));
+            }
+
+            if (configureClient is null)
+            {
+                throw new ArgumentNullException(nameof(configureClient));
+            }
+
+            var httpClient = new HttpClient(handler);
+            configureClient(httpClient);
+
+            return httpClient;
+        }
+    }
+}
---
+++ test/TestableHttpClient.Tests/TestableHttpMessageHandlerExtensionsTests/CreateClientWithConfigurer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Net.Http;
+
+using Moq;
+
+using Xunit;
+
+namespace TestableHttpClient.Tests
+{
+    public partial class TestableHttpMessageHandlerExtensionsTests
+    {
+#nullable disable
+        [Fact]
+        public void CreateClientWithConfigurer_NullTestableHttpMessageHandler_ThrowsArgumentNullException()
+        {
+            TestableHttpMessageHandler sut = null;
+            Action<HttpClient> configureClient = client => { };
+
+            var exception = Assert.Throws<ArgumentNullException>(() => sut.CreateClient(configureClient));
+            Assert.Equal("handler", exception.ParamName);
+        }
+
+        [Fact]
+        public void CreateClientWithConfigurer_NullConfigureAction_ThrowsArgumentNullException()
+        {
+            using var sut = new TestableHttpMessageHandler();
+            Action<HttpClient> configureClient = null;
+
+            var exception = Assert.Throws<ArgumentNullException>(() => sut.CreateClient(configureClient));
+            Assert.Equal("configureClient", exception.ParamName);
+        }
+#nullable restore
+
+        [Fact]
+        public void CreateClientWithConfigurer_ReturnsHttpClientWithHandlerConfigured()
+        {
+            using var sut = new TestableHttpMessageHandler();
+            Action<HttpClient> configureClient = client => { };
+
+            using var client = sut.CreateClient(configureClient);
+
+            var handler = GetPrivateHandler(client);
+
+            Assert.Same(sut, handler);
+        }
+
+        [Fact]
+        public void CreateClientWithConfigurer_CallsConfigureClientWithClientToReturn()
+        {
+            using var sut = new TestableHttpMessageHandler();
+            Mock<Action<HttpClient>> configureClient = new Mock<Action<HttpClient>>();
+
+            using var client = sut.CreateClient(configureClient.Object);
+
+            configureClient.Verify(x => x.Invoke(client), Times.Once);
+        }
+    }
+}
