9d46573454879d48349ef043c0621ad358f2132b
David Perfors <dnperfors@gmail.com>
2022-03-13 20:47:49 +0100

    dotnet format

--- test/TestableHttpClient.IntegrationTests/GlobalUsings.cs
+++ test/TestableHttpClient.IntegrationTests/GlobalUsings.cs
@@ -1,9 +1,7 @@
-﻿global using System;
-global using System.Collections.Generic;
-global using System.Text;
-global using System.Net;
+﻿global using System.Net;
 global using System.Net.Http;
 global using System.Net.Http.Headers;
+global using System.Text;
 global using System.Threading.Tasks;
 
 global using NFluent;
