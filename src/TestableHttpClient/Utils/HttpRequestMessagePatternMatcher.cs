b1cf4df0fccbfdddf0b018bee0eebc76d9826c86
David Perfors <dnperfors@gmail.com>
2024-06-14 20:34:56 +0200

    First part of httprequestmessagepattern

---
+++ src/TestableHttpClient/Utils/HttpRequestMessagePatternMatcher.cs
@@ -0,0 +1,30 @@
+﻿namespace TestableHttpClient.Utils;
+
+internal class HttpRequestMessagePatternMatcher
+{
+    private readonly HttpRequestMessagePatternMatchingOptions options = new();
+    public HttpRequestMessagePatternMatchResult Match(IEnumerable<HttpRequestMessage> requests, HttpRequestMessagePattern pattern)
+    {
+        HttpRequestMessagePatternMatchResult result = new();
+
+        foreach (HttpRequestMessage request in requests)
+        {
+            HttpRequestMessagePatternMatchingResult match = pattern.Matches(request, options);
+            if (match.All)
+            {
+                result.MatchedRequests.Add(request);
+            }
+            else
+            {
+                result.UnmatchedRequests.Add(request);
+            }
+        }
+        return result;
+    }
+}
+
+internal class HttpRequestMessagePatternMatchResult
+{
+    public List<HttpRequestMessage> MatchedRequests { get; } = new List<HttpRequestMessage>();
+    public List<HttpRequestMessage> UnmatchedRequests { get; } = new List<HttpRequestMessage>();
+}
---
+++ test/TestableHttpClient.Tests/Utils/HttpRequestMessagePatternMatcherTests.cs
@@ -0,0 +1,48 @@
+﻿using FluentAssertions;
+
+using TestableHttpClient.Utils;
+
+namespace TestableHttpClient.Tests.Utils;
+
+public class HttpRequestMessagePatternMatcherTests
+{
+    [Fact]
+    public void MultipleRequests_MatchingAnyPattern_ReturnsAllRequests()
+    {
+        HttpRequestMessage[] requestMessages = [
+            new HttpRequestMessage(HttpMethod.Get, "https://localhost/get"),
+            new HttpRequestMessage(HttpMethod.Post, "https://localhost/post"),
+            new HttpRequestMessage(HttpMethod.Options, "https://localhost/options")
+            ];
+
+        HttpRequestMessagePattern pattern = new();
+
+        HttpRequestMessagePatternMatcher matcher = new();
+
+        HttpRequestMessagePatternMatchResult result = matcher.Match(requestMessages, pattern);
+
+        result.MatchedRequests.Should().BeEquivalentTo(requestMessages);
+        result.UnmatchedRequests.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void MultipleRequests_MatchingSpecificHttpMethod_ReturnsMatchinRequests()
+    {
+        HttpRequestMessage getRequest = new(HttpMethod.Get, "https://localhost/get");
+        HttpRequestMessage postRequest = new(HttpMethod.Post, "https://localhost/post");
+        HttpRequestMessage optionsRequest = new(HttpMethod.Options, "https://localhost/options");
+        HttpRequestMessage[] requestMessages = [ getRequest, postRequest, optionsRequest ];
+
+        HttpRequestMessagePattern pattern = new()
+        {
+            Method = Value.Exact(HttpMethod.Post)
+        };
+
+        HttpRequestMessagePatternMatcher matcher = new();
+
+        HttpRequestMessagePatternMatchResult result = matcher.Match(requestMessages, pattern);
+
+        result.MatchedRequests.Should().BeEquivalentTo([postRequest]);
+        result.UnmatchedRequests.Should().BeEquivalentTo([getRequest, optionsRequest]);
+    }
+}
