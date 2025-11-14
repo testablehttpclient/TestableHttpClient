be3a5cd03b443204d765ebe02099ab9d8d223c74
David Perfors <dnperfors@gmail.com>
2025-11-14 22:56:28 +0100

    Refactor tests to test methods based on the HttpRequestMessageAsserter, so that will be easier to refactor.

--- test/TestableHttpClient.Tests/HttpRequestMessageAsserterTests/WithFilter.cs
+++ test/TestableHttpClient.Tests/HttpRequestMessageAsserterTests/WithFilter.cs
@@ -1,28 +1,7 @@
-﻿namespace TestableHttpClient.Tests;
+﻿namespace TestableHttpClient.Tests.HttpRequestMessageAsserterTests;
 
-public class HttpRequestMessageAsserterTests
+public sealed class WithFilter
 {
-    [Fact]
-    public void Constructor_NullRequestList_ThrowsArgumentNullException()
-    {
-        Assert.Throws<ArgumentNullException>(() => new HttpRequestMessageAsserter(null!));
-    }
-
-    [Fact]
-    public void Constructor_NullOptions_SetsDefault()
-    {
-        HttpRequestMessageAsserter sut = new([], null);
-        Assert.NotNull(sut.Options);
-    }
-
-    [Fact]
-    public void Constructor_NotNullOptions_SetsOptions()
-    {
-        TestableHttpMessageHandlerOptions options = new();
-        HttpRequestMessageAsserter sut = new([], options);
-        Assert.Same(options, sut.Options);
-    }
-
     [Fact]
     public void WithFilter_NullPredicate_ThrowsArgumentNullException()
     {
---
+++ test/TestableHttpClient.Tests/HttpRequestMessageAsserterTests/WithRequestUri.cs
@@ -0,0 +1,78 @@
+﻿using NSubstitute;
+
+using TestableHttpClient.Tests.HttpRequestMessagesCheckExtensionsTests;
+
+namespace TestableHttpClient.Tests.HttpRequestMessageAsserterTests;
+
+public sealed class WithRequestUri
+{
+    [Fact]
+    public void NullPattern_ThrowsArgumentNullException()
+    {
+        HttpRequestMessageAsserter sut = new([]);
+
+        var exception = Assert.Throws<ArgumentNullException>(() => sut.WithRequestUri(null!));
+
+        Assert.Equal("pattern", exception.ParamName);
+    }
+
+    [Fact]
+    public void EmptyPattern_ThrowsArgumentException()
+    {
+        HttpRequestMessageAsserter sut = new([]);
+
+        var exception = Assert.Throws<ArgumentException>(() => sut.WithRequestUri(string.Empty));
+
+        Assert.Equal("pattern", exception.ParamName);
+    }
+
+    [Fact]
+    public void WithPatternMatchingARequest_DoesNotThrow()
+    {
+        using HttpRequestMessage request = new(HttpMethod.Get, "https://example.com");
+        HttpRequestMessageAsserter sut = new([request]);
+
+        sut.WithRequestUri("https://example.com/");
+    }
+
+    [Fact]
+    public void WithPatternNotMatchingAnyRequest_ThrowsHttpRequestMessageAssertionException()
+    {
+        using HttpRequestMessage request = new(HttpMethod.Get, "https://example.com");
+        HttpRequestMessageAsserter sut = new([request]);
+
+        Assert.Throws<HttpRequestMessageAssertionException>(() => sut.WithRequestUri("https://no-op.com/"));
+    }
+
+
+    [Fact]
+    public void WithPatternNotMatchingAnyRequestWithCustomOptions_ThrowsHttpRequestMessageAssertionException()
+    {
+        TestableHttpMessageHandlerOptions options = new();
+        options.UriPatternMatchingOptions.HostCaseInsensitive = false;
+
+        using HttpRequestMessage request = new(HttpMethod.Get, "https://example.com");
+
+        HttpRequestMessageAsserter sut = new([request], options);
+
+        Assert.Throws<HttpRequestMessageAssertionException>(() => sut.WithRequestUri("https://EXAMPLE.com/"));
+    }
+
+    [Fact]
+    public void WithPatternMatchingASingleRequest_DoesNotThrow()
+    {
+        using HttpRequestMessage request = new(HttpMethod.Get, "https://example.com");
+        HttpRequestMessageAsserter sut = new([request]);
+
+        sut.WithRequestUri("https://example.com/", 1);
+    }
+
+    [Fact]
+    public void WithPatternMatchingMultipleRequestsButOnlyOneReceived_ThrowsHttpRequestMessageAssertionException()
+    {
+        using HttpRequestMessage request = new(HttpMethod.Get, "https://example.com");
+        HttpRequestMessageAsserter sut = new([request]);
+
+        Assert.Throws<HttpRequestMessageAssertionException>(() => sut.WithRequestUri("https://no-op.com/", 2));
+    }
+}
