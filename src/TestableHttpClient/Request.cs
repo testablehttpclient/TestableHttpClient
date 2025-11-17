b8cf06603c7099097f42126532831805c546e517
David Perfors <dnperfors@gmail.com>
2025-11-17 13:24:02 +0100

    Move logic for assert WithHeader to custom Request class.

--- src/TestableHttpClient/Request.cs
+++ src/TestableHttpClient/Request.cs
@@ -13,7 +13,38 @@ internal record Request : IEquatable<HttpRequestMessage>
     public UriPattern? RequestUri { get; init; }
     public Version? HttpVersion { get; init; }
 
+    public List<string>? HeaderNames { get; init; }
+    public Dictionary<string, string>? HeaderValues { get; init; }
+
     public string? Content { get; init; }
+
+    public Request AddHeader(string headerName)
+    {
+        if (HeaderNames is null)
+        {
+            List<string> headerNames = [headerName];
+            return this with { HeaderNames = headerNames };
+        }
+        else
+        {
+            HeaderNames.Add(headerName);
+            return this;
+        }
+    }
+
+    public Request AddHeader(string headerName, string headerValue)
+    {
+        if (HeaderValues is null)
+        {
+            Dictionary<string, string> headerValues = new() { [headerName] = headerValue };
+            return this with { HeaderValues = headerValues };
+        }
+        else
+        {
+            HeaderValues[headerName] = headerValue;
+            return this;
+        }
+    }
 
     public bool Equals(HttpRequestMessage? other)
     {
@@ -37,9 +68,31 @@ internal record Request : IEquatable<HttpRequestMessage>
             return false;
         }
 
+        if (HeaderNames is not null)
+        {
+            foreach (var headerName in HeaderNames)
+            {
+                if (!other.Headers.HasHeader(headerName) && (other.Content is null || !other.Content.Headers.HasHeader(headerName)))
+                {
+                    return false;
+                }
+            }
+        }
+
+        if (HeaderValues is not null)
+        {
+            foreach (var keyValuePair in HeaderValues)
+            {
+                if (!other.Headers.HasHeader(keyValuePair.Key, keyValuePair.Value) && (other.Content is null || !other.Content.Headers.HasHeader(keyValuePair.Key, keyValuePair.Value)))
+                {
+                    return false;
+                }
+            }
+        }
+
         if (Content is not null)
         {
-            if(other.Content is null)
+            if (other.Content is null)
             {
                 return false;
             }
