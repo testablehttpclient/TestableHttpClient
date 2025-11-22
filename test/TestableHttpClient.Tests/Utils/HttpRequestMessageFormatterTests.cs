2843fec6d10ab024edec816e440b0df949ddaf47
David Perfors <dnperfors@gmail.com>
2025-11-22 16:30:13 +0100

    Add some basic tests for formatting httprequestmessages

---
+++ test/TestableHttpClient.Tests/Utils/HttpRequestMessageFormatterTests.cs
@@ -0,0 +1,57 @@
+﻿using System.IO;
+using System.Runtime.CompilerServices;
+
+using TestableHttpClient.Utils;
+
+namespace TestableHttpClient.Tests.Utils;
+
+public sealed class HttpRequestMessageFormatterTests
+{
+    private static string FetchTestData(string filename, [CallerFilePath] string callerFilePath = "")
+    {
+        string directory = Path.GetDirectoryName(callerFilePath)!;
+        string filePath = Path.Combine(directory, "HttpRequestMessageFormatterTestData", filename);
+        filePath += ".verified.http";
+        return File.ReadAllText(filePath);
+    }
+    [Fact]
+    public void Format_NullRequest_CreatesExpectedString()
+    {
+        HttpRequestMessage? request = null;
+
+        var result = HttpRequestMessageFormatter.Format(request, HttpRequestMessageFormatOptions.All);
+
+        Assert.Equal("null", result);
+    }
+
+    [Theory]
+    [InlineData("1.0")]
+    [InlineData("1.1")]
+    [InlineData("2.0")]
+    [InlineData("3.0")]
+    public void Format_SimpleGetRequest_CreatesExpectedString(string version)
+    {
+        using HttpRequestMessage request = new(HttpMethod.Get, "https://example.com")
+        {
+            Version = Version.Parse(version)
+        };
+        string result = HttpRequestMessageFormatter.Format(request, HttpRequestMessageFormatOptions.All);
+
+        string expected = FetchTestData($"simple_get_request_version_{version}");
+
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void Format_SimplePostRequestWithHttpVersion2_CreatesExpectedString()
+    {
+        using HttpRequestMessage request = new(HttpMethod.Post, "https://example.com");
+        request.Content = new StringContent("Hello, World!");
+        request.Content.Headers.ContentLength = 13;
+        string result = HttpRequestMessageFormatter.Format(request, HttpRequestMessageFormatOptions.All);
+
+        string expected = FetchTestData("simple_post_request");
+
+        Assert.Equal(expected, result);
+    }
+}
