30a832e4141e50d2ad4828ef4fd2260dd064b304
David Perfors <dnperfors@gmail.com>
2026-06-03 21:50:28 +0200

    Make methods in IRoutingResponseBuilder chainable (#331)

--- src/TestableHttpClient/Utils/RoutingResponseBuilder.cs
+++ src/TestableHttpClient/Utils/RoutingResponseBuilder.cs
@@ -6,6 +6,15 @@ internal sealed class RoutingResponseBuilder : IRoutingResponseBuilder
 {
     public RoutingResponse RoutingResponse { get; } = new();
 
-    public void MapFallBackResponse(IResponse fallBackResponse) => RoutingResponse.FallBackResponse = fallBackResponse;
-    public void Map(string route, IResponse response) => RoutingResponse.ResponseMap.Add(UriPatternParser.Parse(route), response);
+    public IRoutingResponseBuilder MapFallBackResponse(IResponse fallBackResponse)
+    {
+        RoutingResponse.FallBackResponse = fallBackResponse;
+        return this;
+    }
+
+    public IRoutingResponseBuilder Map(string route, IResponse response)
+    {
+        RoutingResponse.ResponseMap.Add(UriPatternParser.Parse(route), response);
+        return this;
+    }
 }
